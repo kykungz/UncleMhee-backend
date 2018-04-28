47bae55927fc5df1500f96e8dce7b7bd96533d87
chinatip <chinatip.v@ku.th>
2018-04-28 22:02:36 +0700

    Add taxi, report, service and bill

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,11 @@ router.get('/', (req, res, next) => {
 })
 
 router.get('/taxi', (req, res, next) => {
-  console.log(req.body)
   const ref = firebase.database().ref('/scb').push({
     catagory: 'taxi',
-    content: ''
+    content: req.headers.body,
+    roomNumber: Math.round(Math.random() * 200),
+    done: false
   })
 
   setTimeout(() => {
@@ -36,20 +37,61 @@ router.get('/print', (req, res, next) => {
 router.get('/fix', (req, res, next) => {
   const ref = firebase.database().ref('/scb').push({
     catagory: 'fix',
-    content: ''
+    content: req.headers.body,
+    roomNumber: Math.round(Math.random() * 200),
+    done: false
   })
+
+  setTimeout(() => {
+    ref.remove()
+  }, (Math.random() * 8 + 10) * 1000)
 
   res.send({ success: true })
 })
 
 router.get('/report', (req, res, next) => {
   const ref = firebase.database().ref('/scb').push({
-    catagory: 'fix',
-    content: ''
+    catagory: 'report',
+    content: req.headers.body,
+    roomNumber: Math.round(Math.random() * 200),
+    done: false
   })
+
+  setTimeout(() => {
+    ref.remove()
+  }, (Math.random() * 8 + 10) * 1000)
 
   res.send({ success: true })
 })
 
+router.get('/service', (req, res, next) => {
+  const ref = firebase.database().ref('/scb').push({
+    catagory: 'service',
+    content: req.headers.body,
+    roomNumber: Math.round(Math.random() * 200),
+    done: false
+  })
+
+  setTimeout(() => {
+    ref.remove()
+  }, (Math.random() * 8 + 10) * 1000)
+
+  res.send({ success: true })
+})
+
+router.get('/bill', (req, res, next) => {
+  const ref = firebase.database().ref('/scb').push({
+    catagory: 'bill',
+    content: req.headers.body,
+    roomNumber: Math.round(Math.random() * 200),
+    done: false
+  })
+
+  setTimeout(() => {
+    ref.remove()
+  }, (Math.random() * 8 + 10) * 1000)
+
+  res.send({ success: true })
+})
 
 module.exports = router
