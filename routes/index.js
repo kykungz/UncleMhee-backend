c351f2dbfaec238388579a72a5bd42470d7bc9e3
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-04-28 20:32:31 +0700

    abc

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,46 @@ router.get('/', (req, res, next) => {
 router.get('/taxi', (req, res, next) => {
   const ref = firebase.database().ref('/scb').push({
     catagory: 'taxi',
-    destination: ''
+    content: ''
   })
 
   setTimeout(() => {
     ref.remove()
-  }, 5 * 1000)
+  }, (Math.random() * 8 + 10) * 1000)
 
-  res.send({
-    taxi: true
-  })
+  res.send({ success: true })
 })
+
+router.get('/print', (req, res, next) => {
+  const ref = firebase.database().ref('/scb').push({
+    catagory: 'print',
+    content: ''
+  })
+
+  setTimeout(() => {
+    ref.remove()
+  }, (Math.random() * 8) * 1000)
+
+  res.send({ success: true })
+})
+
+router.get('/fix', (req, res, next) => {
+  const ref = firebase.database().ref('/scb').push({
+    catagory: 'fix',
+    content: ''
+  })
+
+  res.send({ success: true })
+})
+
+router.get('/report', (req, res, next) => {
+  const ref = firebase.database().ref('/scb').push({
+    catagory: 'fix',
+    content: ''
+  })
+
+  res.send({ success: true })
+})
+
 
 module.exports = router
